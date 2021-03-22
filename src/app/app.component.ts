f8cf098ebf1712a708bd46403a312a3200da2aba
ricardo.neto <ricardo.neto@basis.com.br>
2021-03-21 22:07:42 -0300

    Iniciando Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
-
+import { Planeta } from 'src/model/planeta';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'solar-system-population-app';
+  title = 'Solar System Population App';
+
+  populacao: Planeta[] = [
+    {
+      'nome_planeta': 'Terra',
+      'populacao_humanos': 200000000,
+      'populacao_alienigenas': 153000,
+      'populacao_asgardianos': 500,
+      'populacao_ancioes': 32111,
+      'populacao_celestiais': 12,
+    },
+    {
+      'nome_planeta': 'Marte',
+      'populacao_humanos': 100,
+      'populacao_alienigenas': 500000000,
+      'populacao_asgardianos': 333,
+      'populacao_ancioes': 123,
+      'populacao_celestiais': 7654,
+    },
+    {
+      'nome_planeta': 'Asgard',
+      'populacao_humanos': 300,
+      'populacao_alienigenas': 10000,
+      'populacao_asgardianos': 5000,
+      'populacao_ancioes': 43534,
+      'populacao_celestiais': 34534,
+    },
+    {
+      'nome_planeta': 'Saturno',
+      'populacao_humanos': 0,
+      'populacao_alienigenas': 3214,
+      'populacao_asgardianos': 5444,
+      'populacao_ancioes': 5555,
+      'populacao_celestiais': 6666,
+    },
+    {
+      'nome_planeta': 'Jupiter',
+      'populacao_humanos': 432432,
+      'populacao_alienigenas': 43455,
+      'populacao_asgardianos': 6666,
+      'populacao_ancioes': 12,
+      'populacao_celestiais': 123,
+    }
+  ];
+
+  onDelete(row: Planeta) {
+    this.populacao.splice(this.populacao.indexOf(row), 1);
+  }
+
 }
---
+++ src/model/planeta.ts
@@ -0,0 +1,8 @@
+export class Planeta {
+    nome_planeta: string;
+    populacao_humanos: number;
+    populacao_alienigenas: number;
+    populacao_asgardianos: number;
+    populacao_ancioes: number;
+    populacao_celestiais: number;
+}
